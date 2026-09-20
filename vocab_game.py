4d1afc43674d85632adea06d63dd5c5125d897ce
402-40-Thanapat <56323@yupparaj.ac.th>
2026-09-20 13:36:31 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -78,7 +78,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
 st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
 
 # 2. แถบแสดงเวลานับถอยหลัง
-if "start" in st.session_state and not st.session_state.get("is_ended", False):
+if st.session_state.get("game_started", False) and not st.session_state.get("is_ended", False):
     time_left = int(30 - (time.time() - st.session_state.start))
 
     if time_left > 0:
