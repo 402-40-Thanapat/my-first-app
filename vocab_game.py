04752a38d018663b157520a2f7c10dd72f808f8b
402-40-Thanapat <56323@yupparaj.ac.th>
2026-09-20 13:03:13 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -93,22 +93,22 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 st.divider()
 
 # 3. ช่องรับคำตอบ (ใช้ value ผูกกับตัวแปรตรงๆ เพื่อสั่งเคลียร์ได้)
-ans1 = st.text_input(
-    "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
-    value=st.session_state.ans1_val,
-)
-ans2 = st.text_input(
-    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
-    value=st.session_state.ans2_val,
-)
-ans3 = st.text_input(
-    "ข้อ 3: I read `b _ _ _ s` in my free time. 📚",
-    value=st.session_state.ans3_val,
-)
-ans4 = st.text_input(
-    "ข้อ 4: Monkeys like to eat `b _ _ _ _ a`. 🍌",
-    value=st.session_state.ans4_val,
-)
+  ans1 = st.text_input(
+      "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
+      value=st.session_state.ans1_val,
+  )
+  ans2 = st.text_input(
+      "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
+      value=st.session_state.ans2_val,
+  )
+  ans3 = st.text_input(
+      "ข้อ 3: I read `b _ _ _ s` in my free time. 📚",
+      value=st.session_state.ans3_val,
+  )
+  ans4 = st.text_input(
+      "ข้อ 4: Monkeys like to eat `b _ _ _ _ a`. 🍌",
+      value=st.session_state.ans4_val,
+  )
 
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
